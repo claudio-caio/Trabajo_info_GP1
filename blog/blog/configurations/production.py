e5611155fbf951ea84d5f1975312efd1d3ec6773
Claudio Tomadin <caioalegres@gmail.com>
2025-12-13 18:53:21 -0300

    Elimina archivos de configuración y scripts obsoletos, actualiza la configuración de la base de datos para usar MySQL en producción

--- blog/blog/configurations/production.py
+++ blog/blog/configurations/production.py
@@ -9,18 +9,14 @@ DEBUG = False
 # TODO: Cambiar la config de la base de datos para prod
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-
-        # 'ENGINE': 'django.db.backends.postgresql',
-        # 'ENGINE': 'django.db.backends.mysql',
-
-        # 'NAME': os.getenv('DB_NAME'),
-        # 'USER': os.getenv('DB_USER'),
-        # 'PASSWORD': os.getenv('DB_PASSWORD'),
-        # 'HOST': os.getenv('DB_HOST'),
-        # 'PORT': os.getenv('DB_PORT'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
+
 
 os.environ['DJANGO_PORT'] = '8080'
